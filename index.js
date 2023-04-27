4395c5a8205b54203f98db46615f115feae381a5
iulia22A <iuliavasile2207@gmail.com>
2023-04-27 21:59:17 +0300

    js_final_commit

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+// Adding an event listener to the document that listens for mousemove events, and calls the function named "parallax" when a mousemove event is detected
 document.addEventListener("mousemove", parallax);
 
+// Defining a function named "parallax" that takes an event object "e" as its parameter
 function parallax(e) {
-  this.querySelectorAll(".move_this").forEach((layer) => {
-    const speed = layer.getAttribute("data-speed");
+// Selecting all the HTML elements that have a class of "move_this", and looping through them using forEach method
+this.querySelectorAll(".move_this").forEach((layer) => {
+// Getting the value of the "data-speed" attribute of each selected element and assigning it to a variable called "speed"
+const speed = layer.getAttribute("data-speed");
 
-    const x = (window.innerWidth - e.pageX * speed) / 100;
-    const y = (window.innerHeight - e.pageY * speed) / 100;
+// Calculating the horizontal and vertical position of the mouse pointer relative to the window, and adjusting the values based on the value of "speed"
+const x = (window.innerWidth - e.pageX * speed) / 100;
+const y = (window.innerHeight - e.pageY * speed) / 100;
 
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-  });
+// Setting the transform property of the current element to translate it horizontally and vertically by the calculated values
+layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+});
 }
+
+// The above code creates a parallax effect on the elements with class "move_this" as the user moves the mouse.
